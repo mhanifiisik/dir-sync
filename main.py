817a6a111cd74108ad4d77f6e890fe0b1d6b1152
mehanisik <mehmethanifiisik64@gmail.com>
2025-04-03 17:52:57 +0200

    feat: add command line argument parsing and directory synchronization functionality

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import logging
-
+import os
+import argparse
+import time
+from datetime import datetime
 
 def setup_logging():
     """
@@ -23,17 +26,72 @@ def setup_logging():
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
 
+
+
+
+
+def read_the_arguments_from_command_line(logger):
+    """
+    Read the arguments from the command line
+    """
+
+    parser = argparse.ArgumentParser(description='Synchronize directories')
+    parser.add_argument('source', type=str, nargs='?', help='Source directory')
+    parser.add_argument('replica', type=str, nargs='?', help='Replica directory')
+    parser.add_argument('interval', type=int, nargs='?', help='Interval in seconds')
+    args = parser.parse_args()
+
+    logger.info(f"Source directory: {args.source}")
+    logger.info(f"Replica directory: {args.replica}")
+    logger.info(f"Interval: {args.interval}")
+    return args.source, args.replica, args.interval
+
+
+
+def check_the_folders_exist(source_dir, replica_dir, logger):
+
+    """
+    Check if the source and replica directories exist
+    """
+
+    if not os.path.exists(source_dir):
+        logger.error(f"Source directory does not exist: {source_dir}")
+        return False
+    if not os.path.exists(replica_dir):
+        logger.error(f"Replica directory does not exist: {replica_dir}")
+        os.makedirs(replica_dir)
+        logger.info(f"Created replica directory: {replica_dir}")
+        return False
+    return True
+
+
+
+
+
+
+
+def sync_directories(source_dir, replica_dir , interval, logger):
+
+    """
+    One way folder syncrenization from source to replica folders periodically
+    """
+    logger.info(f"Synchronizing directories at {datetime.now()}")
+    time.sleep(interval)
+    logger.info(f"Synchronization completed at {datetime.now()}")
+
+
+
 def main():
     """
     main function for the application
     """
     setup_logging()
+    source_dir, replica_dir, interval = read_the_arguments_from_command_line(logging)
+    check_the_folders_exist(source_dir, replica_dir, logging)
+    sync_directories(source_dir, replica_dir, interval, logging)
 
-    logging.info('Starting the synchronization process')
-    source_dir = 'source_dir'
-    replica_dir = 'replica_dir'
-    logging.info(f'Using source directory: {source_dir}')
-    logging.info(f'Using replica directory: {replica_dir}')
+
+
 
 if __name__ == "__main__":
     main()
